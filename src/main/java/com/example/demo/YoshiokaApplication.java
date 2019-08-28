dffda34ccdf3b8c931faa90885f56c6e463f250b
yukihiro-yokogawa <yukihiro.yokogawa@rakus-partners.co.jp>
2019-08-28 10:50:25 +0900

    first commit bikeクラスのrunメソッド

---
+++ src/main/java/com/example/demo/YoshiokaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoshiokaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoshiokaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/bike.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+public class bike {
+
+	int speed = 100;
+	int pedal = 2;
+	int tire = 2;
+	int chare = 1;
+	int money = 150000;
+
+	public void run() {
+		System.out.println("ロードバイクをこぐ吉岡かっこいい");
+	}
+	
+	}
